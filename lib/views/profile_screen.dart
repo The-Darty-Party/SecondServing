52bab8c70360cc96e2198f2959563effd29de920
Mustafa_alsoffi <alsoffimustafa@gmail.com>
2023-05-16 15:24:04 +0800

    Add const everywhere

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -1,9 +1,6 @@
 //profile_screen.dart
 import 'package:flutter/material.dart';
-
-import '../main.dart';
 import '../services/firebase_auth_service.dart';
-
 
 class ProfileScreen extends StatelessWidget {
   final String user;
@@ -15,14 +12,14 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: const Text('Profile'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            CircleAvatar(
+            const CircleAvatar(
               radius: 50,
               backgroundColor: Colors
                   .grey, // Set a background color for the anonymous user icon
@@ -32,23 +29,23 @@ class ProfileScreen extends StatelessWidget {
                 color: Colors.white,
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Text(
               'Username: $user',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             Text(
               'Email: $email',
-              style: TextStyle(fontSize: 16),
+              style: const TextStyle(fontSize: 16),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 firebaseAuth.signOut();
                 Navigator.popUntil(context, (route) => route.isFirst);
               },
-              child: Text('Sign Out'),
+              child: const Text('Sign Out'),
             ),
           ],
         ),
